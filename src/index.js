d5f7904562e9e9a96ed4ac5c586805e2a536603b
yazen firas <psyazen3@gmail.com>
2020-07-04 21:53:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-/*let addToy = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-    } else {
-      toyForm.style.display = "none";
-    }
-  });
-});
-*/
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
